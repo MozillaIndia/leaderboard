b9c0de311f1f95ee18ff84068c359cf9e5686705
Sankha Narayan Guria <sankha93@gmail.com>
2013-08-07 00:00:46 +0530

    display verfied bugs as well

--- js/dashboard.js
+++ js/dashboard.js
@@ -55,5 +55,5 @@ for (var i = 0; i < users.length; i++) {
   };
   bugzilla.countBugs({email1: email,
                       email1_assigned_to: 1,
-                      status: 'RESOLVED'}, resendFn(name, email, hash, access));
+                      status: ['RESOLVED', 'VERIFIED']}, resendFn(name, email, hash, access));
 }
