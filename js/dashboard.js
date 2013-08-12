5fd925919a88176c0823fde3ac8a13aac1d7761c
Girish Sharma <scrapmachines@gmail.com>
2013-08-13 04:06:47 +0530

    Make the dashboard seem faster.
    This is the first step for converting things to localStorage - lazy load
    to dom .

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,22 +1,27 @@
 var bugzilla = createClient();
-$('#list').hide();
 var details = [];
+var pushed = {};
+var count = 0;;
 
-function displayResults() {
-  if(users.length == details.length) {
-    details.sort(function (a, b) { return b.fixed - a.fixed; });
-    for(var i = 0; i < users.length; i++) {
-      $('#list').append('<tr>' +
-        '<td><img class="avatar" src="http://www.gravatar.com/avatar/' + details[i].hash + '?s=48"></td>' +
-        '<td><a href="mailto:' + details[i].email + '">' + details[i].name + '</a></td>' +
-        '<td align="center"><a target="_blank" href="https://bugzilla.mozilla.org/buglist.cgi?quicksearch=ALL%20assignee%3A' + details[i].email + '"><span class="badge">' + details[i].total + '</span></a></td>' +
-        '<td align="center"><span class="badge">' + details[i].fixed + '</span></td>' +
-        '<td align="center">' + details[i].access + '</td>' +
-        '<td align="right">' + details[i].components + '</td>' +
-        '</tr>');
+function sortResults() {
+  var table = document.querySelector('#list tbody');
+
+  var items = table.childNodes;
+  var itemsArr = [];
+  for (var i in items) {
+    if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
+      itemsArr.push(items[i]);
     }
-    $('#list').show();
-    $('#loading').hide();
+  }
+
+  itemsArr.sort(function(a, b) {
+    a = details[pushed[a.id]].fixed;
+    b = details[pushed[b.id]].fixed;
+    return a > b ? -1 : 1;
+  });
+
+  for (i = 0; i < itemsArr.length; ++i) {
+    table.appendChild(itemsArr[i]);
   }
 }
 
@@ -40,68 +45,28 @@ for (var i = 0; i < users.length; i++) {
       break;
   }
 
-  var countAssigned = function(name, email, hash, access) {
-    return function(msg, fixed) {
-      bugzilla.countBugs({email1: email,
-                          email1_assigned_to: 1}, countComponent(name, email, hash, access, fixed));
-    };
-  };
+  $('#list tbody').append('<tr id="' + email.replace(/[.@]/g, "") + '">' +
+    '<td><img class="avatar" src="http://www.gravatar.com/avatar/' + hash + '?s=48"></td>' +
+    '<td><a href="mailto:' + email + '">' + name + '</a></td>' +
+    '<td align="center"><a target="_blank" href="https://bugzilla.mozilla.org/buglist.cgi?quicksearch=ALL%20assignee%3A' + email + '"><span class="badge assigned" value=""></span></a></td>' +
+    '<td align="center"><span class="badge fixed" value=""></span></td>' +
+    '<td align="center">' + access + '</td>' +
+    '<td align="right" class="component"></td>' +
+    '</tr>');
 
-  var countComponent = function(name, email, hash, access, fixed) {
-    return function(msg, assigned) {
-      bugzilla.countBugsX({
-        x_axis_field: "product",
-        y_axis_field: "component",
-        "field0-0-0": "attachment.is_patch",
-        "type0-0-0": "equals",
-        "value0-0-0": 1,
-        "field0-2-0": "attachment.attacher",
-        "type0-2-0": "equals",
-        "value0-2-0": email,
-        "field0-1-0": "flagtypes.name",
-        "type0-1-0": "contains",
-        "value0-1-0": "+",
-        email1: email,
-        email1_assigned_to: 1,
-        status: ['RESOLVED', 'VERIFIED'],
-        resolution: ['FIXED']
-      }, loaderFn(name, email, hash, access, fixed, assigned));
-    };
-  };
+  pushed[email.replace(/[.@]/g, "")] = i;
+  details.push({
+    name: name,
+    email: email,
+    hash: hash,
+    access: access
+  });
 
-  var loaderFn = function(name, email, hash, access, fixed, assigned) {
-    return function(msg, components) {
-      var obj = { name: name,
-                  email: email,
-                  hash: hash,
-                  access: access,
-                  fixed: fixed,
-                  components: "",
-                  total: assigned };
-      var data = [];
-      if (components.data.length) {
-        data = data.concat.apply(data, components.data);
-        var largest = Math.max.apply(Math, data);
-        var index = data.indexOf(largest);
-        if (largest > 0) {
-          obj.components = ((components.x_labels[index%components.x_labels.length] || "") + " :: " +
-                            (components.y_labels[index/components.x_labels.length|0] || ""))
-                            .replace(/(^ :: | :: $)/g, "");
-        }
-      }
-      details.push(obj);
-      displayResults();
-    };
-  };
-
-  // Count fixed bugs
+  // Count fixed
   bugzilla.countBugs({
     "field0-0-0": "attachment.is_patch",
     "type0-0-0": "equals",
     "value0-0-0": 1,
-    "field0-2-0": "attachment.attacher",
-    "type0-2-0": "equals",
-    "value0-2-0": email,
     "field0-1-0": "flagtypes.name",
     "type0-1-0": "contains",
     "value0-1-0": "+",
@@ -109,5 +74,51 @@ for (var i = 0; i < users.length; i++) {
     email1_assigned_to: 1,
     status: ['RESOLVED', 'VERIFIED'],
     resolution: ['FIXED']
-  }, countAssigned(name, email, hash, access));
+  }, function(msg, fixed) {
+    details[pushed[this]].fixed = fixed;
+    $("#" + this + " .fixed").text(fixed).attr("value", fixed);
+    if (++count == users.length) {
+      sortResults();
+    }
+  }.bind(email.replace(/[.@]/g, "")));
+
+  // Count assigned
+  bugzilla.countBugs({
+    email1: email,
+    email1_assigned_to: 1
+  }, function(msg, assigned) {
+    details[pushed[this]].assigned = assigned;
+    $("#" + this + " .assigned").text(assigned).attr("value", assigned);
+  }.bind(email.replace(/[.@]/g, "")));
+
+  // Calculate Component
+  bugzilla.countBugsX({
+    x_axis_field: "product",
+    y_axis_field: "component",
+    "field0-0-0": "attachment.is_patch",
+    "type0-0-0": "equals",
+    "value0-0-0": 1,
+    "field0-1-0": "flagtypes.name",
+    "type0-1-0": "contains",
+    "value0-1-0": "+",
+    email1: email,
+    email1_assigned_to: 1,
+    status: ['RESOLVED', 'VERIFIED'],
+    resolution: ['FIXED']
+  }, function(msg, components) {
+    var data = [];
+    if (components.data.length) {
+      data = data.concat.apply(data, components.data);
+      var largest = Math.max.apply(Math, data);
+      var index = data.indexOf(largest);
+      if (largest > 0) {
+        var component = ((components.x_labels[index%components.x_labels.length] || "") + " :: " +
+                         (components.y_labels[index/components.x_labels.length|0] || ""))
+                          .replace(/(^ :: | :: $)/g, "");
+        $("#" + this + " .component").text(component);
+        details[pushed[this]].component = component;
+      }
+    }
+  }.bind(email.replace(/[.@]/g, "")));
+
 }
