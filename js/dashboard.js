234168e8e59d10e1d1a33e58f55ad87d6f72e82a
Girish Sharma <scrapmachines@gmail.com>
2013-08-13 22:11:40 +0530

    Use local storage to store the data and retrieve-lazy-update it lateron

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,12 @@
 var bugzilla = createClient();
-var details = [];
-var pushed = {};
-var count = 0;;
+var details = JSON.parse(localStorage.getItem("data-details") || '[]');
+var pushed = JSON.parse(localStorage.getItem("data-pushed") || '{}');
+var numFixedRecieved = 0;
+var numAssignedRecieved = 0;
+var numComponentsRecieved = 0;
 
 function sortResults() {
   var table = document.querySelector('#list tbody');
-
   var items = table.childNodes;
   var itemsArr = [];
   for (var i in items) {
@@ -15,9 +16,13 @@ function sortResults() {
   }
 
   itemsArr.sort(function(a, b) {
-    a = details[pushed[a.id]].fixed;
-    b = details[pushed[b.id]].fixed;
-    return a > b ? -1 : 1;
+    try {
+      a = details[pushed[a.id]].fixed;
+      b = details[pushed[b.id]].fixed;
+      return a > b ? -1 : 1;
+    } catch (ex) {
+      return 1;
+    }
   });
 
   for (i = 0; i < itemsArr.length; ++i) {
@@ -25,10 +30,33 @@ function sortResults() {
   }
 }
 
+function maybeUpdateLocalStorage() {
+  if (numComponentsRecieved == users.length &&
+      numAssignedRecieved == users.length &&
+      numFixedRecieved == users.length) {
+    localStorage.removeItem("data-pushed");
+    localStorage.removeItem("data-details");
+    localStorage.setItem("data-pushed", JSON.stringify(pushed));
+    localStorage.setItem("data-details", JSON.stringify(details));
+  }
+}
+
+users.sort(function(a, b) {
+  var trimmedEmail1 = a[1].replace(/[.@]/g, "");
+  var trimmedEmail2 = b[1].replace(/[.@]/g, "");
+  try {
+    var aa = details[pushed[trimmedEmail1]].fixed || 0;
+    var bb = details[pushed[trimmedEmail2]].fixed || 0;
+    return aa < bb ? 1 : -1;
+  } catch (ex) {
+    return -1;
+  }
+});
 for (var i = 0; i < users.length; i++) {
   var hash = md5($.trim(users[i][1]).toLowerCase());
   var name = users[i][0];
   var email = users[i][1];
+  var trimmedEmail = email.replace(/[.@]/g, "");
   var access = "";
   switch(users[i][2]) {
     case 1:
@@ -45,22 +73,26 @@ for (var i = 0; i < users.length; i++) {
       break;
   }
 
-  $('#list tbody').append('<tr id="' + email.replace(/[.@]/g, "") + '">' +
+  var fixed = "", assigned = "", component = "";
+  if (pushed[trimmedEmail] != undefined) {
+    var obj = details[pushed[trimmedEmail]];
+    fixed = obj.fixed || "";
+    assigned = obj.assigned || "";
+    component = obj.component || "";
+  }
+  $('#list tbody').append('<tr id="' + trimmedEmail + '">' +
     '<td><img class="avatar" src="http://www.gravatar.com/avatar/' + hash + '?s=48"></td>' +
     '<td><a href="mailto:' + email + '">' + name + '</a></td>' +
-    '<td align="center"><a target="_blank" href="https://bugzilla.mozilla.org/buglist.cgi?quicksearch=ALL%20assignee%3A' + email + '"><span class="badge assigned" value=""></span></a></td>' +
-    '<td align="center"><span class="badge fixed" value=""></span></td>' +
+    '<td align="center"><a target="_blank" href="https://bugzilla.mozilla.org/buglist.cgi?quicksearch=ALL%20assignee%3A' + email + '"><span class="badge assigned" value="' + assigned + '">' + assigned + '</span></a></td>' +
+    '<td align="center"><span class="badge fixed" value="' + fixed + '">' + fixed + '</span></td>' +
     '<td align="center">' + access + '</td>' +
-    '<td align="right" class="component"></td>' +
+    '<td align="right" class="component">' + component + '</td>' +
     '</tr>');
 
-  pushed[email.replace(/[.@]/g, "")] = i;
-  details.push({
-    name: name,
-    email: email,
-    hash: hash,
-    access: access
-  });
+  if (pushed[trimmedEmail] == undefined) {
+    pushed[trimmedEmail] = details.length;
+    details.push({});
+  }
 
   // Count fixed
   bugzilla.countBugs({
@@ -74,22 +106,30 @@ for (var i = 0; i < users.length; i++) {
     email1_assigned_to: 1,
     status: ['RESOLVED', 'VERIFIED'],
     resolution: ['FIXED']
-  }, function(msg, fixed) {
+  }, function(error, fixed) {
+    if (error) {
+      return;
+    }
     details[pushed[this]].fixed = fixed;
     $("#" + this + " .fixed").text(fixed).attr("value", fixed);
-    if (++count == users.length) {
+    if (++numFixedRecieved == users.length) {
       sortResults();
+      maybeUpdateLocalStorage();
     }
-  }.bind(email.replace(/[.@]/g, "")));
+  }.bind(trimmedEmail));
 
   // Count assigned
   bugzilla.countBugs({
     email1: email,
     email1_assigned_to: 1
-  }, function(msg, assigned) {
+  }, function(error, assigned) {
+    if (error) {
+      return;
+    }
     details[pushed[this]].assigned = assigned;
     $("#" + this + " .assigned").text(assigned).attr("value", assigned);
-  }.bind(email.replace(/[.@]/g, "")));
+    maybeUpdateLocalStorage(++numAssignedRecieved);
+  }.bind(trimmedEmail));
 
   // Calculate Component
   bugzilla.countBugsX({
@@ -105,9 +145,12 @@ for (var i = 0; i < users.length; i++) {
     email1_assigned_to: 1,
     status: ['RESOLVED', 'VERIFIED'],
     resolution: ['FIXED']
-  }, function(msg, components) {
+  }, function(error, components) {
+    if (error) {
+      return;
+    }
     var data = [];
-    if (components.data.length) {
+    if (components && components.data && components.data.length) {
       data = data.concat.apply(data, components.data);
       var largest = Math.max.apply(Math, data);
       var index = data.indexOf(largest);
@@ -119,6 +162,7 @@ for (var i = 0; i < users.length; i++) {
         details[pushed[this]].component = component;
       }
     }
-  }.bind(email.replace(/[.@]/g, "")));
+    maybeUpdateLocalStorage(++numComponentsRecieved);
+  }.bind(trimmedEmail));
 
 }
