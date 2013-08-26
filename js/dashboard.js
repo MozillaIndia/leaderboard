fa9c1c486f4251c0e9a9990701551ecad80352f6
Girish Sharma <scrapmachines@gmail.com>
2013-08-27 02:09:31 +0530

    Display GitHub contribution

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,5 @@
-var bugzilla = createClient();
+var bugzilla = createBugzillaClient();
+var github = createGitHubClient();
 var details = JSON.parse(localStorage.getItem("data-details") || '[]');
 // Sanitize the data stored in localstorage
 for (var i = 0; i < details.length; i++) {
@@ -12,6 +13,8 @@ var pushed = JSON.parse(localStorage.getItem("data-pushed") || '{}');
 var numFixedRecieved = 0;
 var numAssignedRecieved = 0;
 var numComponentsRecieved = 0;
+var gitRecieved = 0;
+var gitNumbers = {};
 
 function sortResults() {
   var table = document.querySelector('#list tbody');
@@ -41,7 +44,8 @@ function sortResults() {
 function maybeUpdateLocalStorage() {
   if (numComponentsRecieved == users.length &&
       numAssignedRecieved == users.length &&
-      numFixedRecieved == users.length) {
+      numFixedRecieved == users.length &&
+      gitRecieved == users.length) {
     localStorage.removeItem("data-pushed");
     localStorage.removeItem("data-details");
     localStorage.setItem("data-pushed", JSON.stringify(pushed));
@@ -68,8 +72,12 @@ for (var i = 0; i < users.length; i++) {
   var name = users[i][0];
   var email = users[i][1];
   var trimmedEmail = email.replace(/[.@]/g, "");
+  gitNumbers[trimmedEmail] = [];
+  if (!users[i][2]) {
+    gitRecieved++;
+  }
   var access = "";
-  switch(users[i][2]) {
+  switch(users[i][3]) {
     case 1:
       access = '<span class="label label-info">Level 1</span>';
       break;
@@ -84,11 +92,12 @@ for (var i = 0; i < users.length; i++) {
       break;
   }
 
-  var fixed = "", assigned = "", component = "";
+  var fixed = "", assigned = "", component = "", git = "";
   if (pushed[trimmedEmail] != undefined) {
     var obj = details[pushed[trimmedEmail]];
     fixed = obj.f > -1 ? obj.f: "";
     assigned = obj.a > -1 ? obj.a: "";
+    git = obj.g > -1 ? obj.g: 0;
     component = obj.c || "";
   }
   buffer += '<tr id="' + trimmedEmail + '">' +
@@ -96,6 +105,7 @@ for (var i = 0; i < users.length; i++) {
     '<td><a href="mailto:' + email + '">' + name + '</a></td>' +
     '<td align="center"><a target="_blank" href="https://bugzilla.mozilla.org/buglist.cgi?quicksearch=ALL%20assignee%3A' + email + '"><span class="badge assigned" value="' + assigned + '">' + assigned + '</span></a></td>' +
     '<td align="center"><span class="badge fixed" value="' + fixed + '">' + fixed + '</span></td>' +
+    '<td align="center"><span class="badge git" value="' + git + '">' + git + '</span></td>' +
     '<td align="center">' + access + '</td>' +
     '<td align="right" class="component">' + component + '</td>' +
     '</tr>';
@@ -108,6 +118,46 @@ for (var i = 0; i < users.length; i++) {
 
 $("#list > tbody").html(buffer);
 
+// Get GitHub stats
+for (var i = 0, repo = mozGitRepos[i][1];
+     mozGitRepos[i] && (repo = mozGitRepos[i][1]) && i < mozGitRepos.length;
+     i++) {
+  github.getContributorList(repo, function(list) {
+    var repoContributorList = {};
+    for (var k = 0, item = list[k]; (item = list[k]) && k < list.length; k++) {
+      repoContributorList[item.login] = 1;
+    }
+    for (var k = 0, login = users[k][2];
+         (users[k] && (login = users[k][2])) || k < users.length; k++) {
+      var trimmedEmail = users[k][1].replace(/[.@]/g, "");
+      if (repoContributorList[login]) {
+        github.getCommitList(this, login, function callback(commitList) {
+          gitNumbers[this].push(commitList.length);
+          if (gitNumbers[this].length == mozGitRepos.length) {
+            gitRecieved++;
+            var sum = eval(gitNumbers[this].join("+"));
+            details.g = sum;
+            $("#" + this + " .git").text(sum).attr("value", sum);
+            maybeUpdateLocalStorage();
+          }
+          return;
+        }.bind(trimmedEmail));
+      }
+      else {
+        gitNumbers[trimmedEmail].push(0);
+        if (gitNumbers[trimmedEmail].length == mozGitRepos.length) {
+          gitRecieved++;
+          var sum = eval(gitNumbers[trimmedEmail].join("+"));
+          details.g = sum;
+          $("#" + trimmedEmail + " .git").text(sum).attr("value", sum);
+          maybeUpdateLocalStorage();
+        }
+      }
+    }
+  }.bind(repo));
+}
+
+// Get Bugzilla stats.
 for (var i = 0; i < users.length; i++) {
   var email = users[i][1];
   var trimmedEmail = email.replace(/[.@]/g, "");
