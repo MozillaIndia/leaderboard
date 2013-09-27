034bf0071fa1ef47da480c26e7f3c4bb945728b3
Soumya Deb <debloper@gmail.com>
2013-09-27 23:06:46 +0530

    Sorted statistics inbound...

--- js/app.js
+++ js/app.js
@@ -8,11 +8,31 @@ var leaderboard = {};
 
   leaderboard.mostActiveOf = function (data) {
     var counts = [], components = [];
-      for (let i in data) {
-        components.push(i);
-        counts.push(data[i]);
-      }
-      return components[counts.indexOf(Math.max.apply(window, counts))];
+    for (var i in data) {
+      components.push(i);
+      counts.push(data[i]);
+    }
+    return components[counts.indexOf(Math.max.apply(window, counts))];
+  }
+
+  leaderboard.sortResults = function () {
+    var table = document.querySelector("#list tbody")
+    ,   items = table.childNodes
+    ,   itemsArr = [];
+
+    for (var i in items) {
+      if (items[i].nodeType == 1) itemsArr.push(items[i]);
+    }
+
+    itemsArr.sort(function(a, b) {
+      a = a.childNodes[3].childNodes[0].innerHTML;
+      b = b.childNodes[3].childNodes[0].innerHTML;
+      return (b-a);
+    });
+
+    for (var i in itemsArr) {
+      table.appendChild(itemsArr[i]);
+    }
   }
 
   $.ajax({
@@ -29,14 +49,18 @@ var leaderboard = {};
       dom += '<tr id="' + item.email + '">' +
         '<td><img class="avatar" src="http://www.gravatar.com/avatar/' + item.gravatar + '?s=48"></td>' +
         '<td><a href="#">' + item.name + '</a></td>' +
-        '<td align="center"><span class="badge assigned" value="' + item.bugzilla.assigned + '">' + item.bugzilla.assigned + '</span></td>' +
-        '<td align="center"><span class="badge fixed" value="' + item.bugzilla.fixed + '">' + item.bugzilla.fixed + '</span></td>' +
+        '<td align="center"><span class="badge assigned">' + item.bugzilla.assigned + '</span></td>' +
+        '<td align="center"><span class="badge fixed">' + item.bugzilla.fixed + '</span></td>' +
         '<td align="center">' + item.level + '</td>' +
         '<td align="right" class="component">' + leaderboard.mostActiveOf(item.components) + '</td>' +
         '</tr>';
     }
 
+    // @TODO: throw this DOM-sorting in the gutter & wash hands...
+    // Sort the stats.json data itself, before putting in the DOM.
     $("#list").append(dom);
+    leaderboard.sortResults();
+
   }).fail(function (error) {
     console.log(error);
   });
