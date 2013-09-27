a9524159337fcba1a41189dbc02cbe9276dab806
Soumya Deb <debloper@gmail.com>
2013-09-27 17:01:45 +0530

    Display contribution data from stats.json

--- js/app.js
+++ js/app.js
@@ -4,7 +4,25 @@
   $.ajax({
     url: "stats.json"
   }).done(function (data) {
-    console.log(data);
+    var dom = "";
+
+    // @TODO:
+    // 0. Turn this into a for..of loop
+    // 1. Use arrow-function for components' weight
+    for (var i in data) {
+      var item = data[i];
+
+      dom += '<tr id="' + item.email + '">' +
+        '<td><img class="avatar" src="http://www.gravatar.com/avatar/' + item.gravatar + '?s=48"></td>' +
+        '<td><a href="#">' + item.name + '</a></td>' +
+        '<td align="center"><span class="badge assigned" value="' + item.bugzilla.assigned + '">' + item.bugzilla.assigned + '</span></td>' +
+        '<td align="center"><span class="badge fixed" value="' + item.bugzilla.fixed + '">' + item.bugzilla.fixed + '</span></td>' +
+        '<td align="center">' + item.level + '</td>' +
+        '<td align="right" class="component">' + JSON.stringify(item.components) + '</td>' +
+        '</tr>';
+    }
+
+    $("#list").append(dom);
   }).fail(function (error) {
     console.log(error);
   });
